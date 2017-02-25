a1320e00a58c5243781030eda37bc00d1ae96334
Joel Brisbin <jbrisbin23@gmail.com>
2017-02-25 01:58:04 -0500

    Added an auto-updating year in the footer. Updated JQuery and Materialize frameworks. Added Slick carousel for the Photography page.

--- js/mainScripts.js
+++ js/mainScripts.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-   $(".button-collapse").sideNav();
-    $(".carousel.carousel-slider").carousel({full_width: true});
-    Materialize.showStaggeredList(".staggered-list");
+    $(".button-collapse").sideNav();
+
+    if($('.photo-carousel').length > 0){
+       $('.photo-carousel').slick({
+          adaptiveHeight: true,
+          mobileFirst: true,
+          autoplay: true,
+          arrows: false,
+          dots: true
+       });
+    }
+    if($('.staggered-list').length > 0) {
+       Materialize.showStaggeredList(".staggered-list");
+    }
 });
