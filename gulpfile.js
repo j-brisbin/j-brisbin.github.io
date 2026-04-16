396bab77ed568b74a99299684c7365e78864eda7
Joel Brisbin <jbrisbin23@gmail.com>
2026-04-16 13:46:44 -0400

    Making Gulp updates.

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,65 @@
-var gulp = require('gulp'),
-   concat = require('gulp-concat'),
-   sourcemaps = require('gulp-sourcemaps'),
-   handlebars = require('gulp-handlebars'),
-   uglify = require('gulp-uglify'),
-   sass = require('gulp-sass');
+const { src, dest, watch, series, parallel } = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
 
-var paths = {
+const paths = {
    scripts: 'js/scripts/*.js',
    scripts_concat_output: 'application.min.js',
    scripts_output: 'js/compiled/',
    scss: 'css/sass/*.scss',
    scss_concat_output: 'application.min.css',
    scss_output: 'css/compiled/',
-   sourcemaps: './',
    vendor_scripts: 'js/scripts/vendor/**/*.js',
    vendor_scripts_concat_output: 'vendor.min.js',
    vendor_scss: 'css/sass/vendor/**/*.scss',
    vendor_scss_concat_output: 'vendor.min.css',
 };
 
-/*Minify js task, compiles js
-files and outputs them to minified JS.*/
-gulp.task('minify-js',function(){
-   return gulp.src(paths.scripts)
-      .pipe(sourcemaps.init())
+/* Minify JS task: compiles JS files and outputs them to minified JS. */
+function minifyJs() {
+   return src(paths.scripts, { sourcemaps: true })
       .pipe(uglify())
       .pipe(concat(paths.scripts_concat_output))
-      .pipe(sourcemaps.write(paths.sourcemaps))
-      .pipe(gulp.dest(paths.scripts_output));
-});
+      .pipe(dest(paths.scripts_output, { sourcemaps: '.' }));
+}
 
-/*Minify scss task, compiles SCSS
-files and outputs them to minified CSS.*/
-gulp.task('minify-scss',function(){
-   return gulp.src(paths.scss)
-      .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
+/* Minify SCSS task: compiles SCSS files and outputs them to minified CSS. */
+function minifyScss() {
+   return src(paths.scss, { sourcemaps: true })
+      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
       .pipe(concat(paths.scss_concat_output))
-      .pipe(sourcemaps.write(paths.sourcemaps))
-      .pipe(gulp.dest(paths.scss_output));
-});
+      .pipe(dest(paths.scss_output, { sourcemaps: '.' }));
+}
 
-/*Minify vendor js task, compiles vendor js
-files and outputs them to minified JS.*/
-gulp.task('minify-vendor-js',function(){
-   return gulp.src(paths.vendor_scripts)
-      .pipe(sourcemaps.init())
+/* Minify vendor JS task: compiles vendor JS files and outputs them to minified JS. */
+function minifyVendorJs() {
+   return src(paths.vendor_scripts, { sourcemaps: true })
       .pipe(uglify())
       .pipe(concat(paths.vendor_scripts_concat_output))
-      .pipe(sourcemaps.write(paths.sourcemaps))
-      .pipe(gulp.dest(paths.scripts_output));
-});
+      .pipe(dest(paths.scripts_output, { sourcemaps: '.' }));
+}
 
-/*Minify vendor scss task, compiles vendor SCSS
-files and outputs them to minified CSS.*/
-gulp.task('minify-vendor-scss',function(){
-   return gulp.src(paths.vendor_scss)
-      .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
+/* Minify vendor SCSS task: compiles vendor SCSS files and outputs them to minified CSS. */
+function minifyVendorScss() {
+   return src(paths.vendor_scss)
+      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
       .pipe(concat(paths.vendor_scss_concat_output))
-      .pipe(gulp.dest(paths.scss_output));
-});
+      .pipe(dest(paths.scss_output));
+}
 
-gulp.task('watch',[]);
+/* Watch task: watches for changes in JS and SCSS files. */
+function watchFiles() {
+   watch(paths.scripts, minifyJs);
+   watch(paths.scss, minifyScss);
+   watch(paths.vendor_scripts, minifyVendorJs);
+   watch(paths.vendor_scss, minifyVendorScss);
+}
 
-gulp.task('default',['minify-js','minify-scss','minify-vendor-js','minify-vendor-scss']);
+// Export tasks for CLI access
+exports['minify-js'] = minifyJs;
+exports['minify-scss'] = minifyScss;
+exports['minify-vendor-js'] = minifyVendorJs;
+exports['minify-vendor-scss'] = minifyVendorScss;
+exports.watch = watchFiles;
+exports.default = parallel(minifyJs, minifyScss, minifyVendorJs, minifyVendorScss);
